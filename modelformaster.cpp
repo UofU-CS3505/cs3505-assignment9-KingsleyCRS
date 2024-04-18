b9342f4b996f4af4acc2c0265948573c67b9e332
jinchaoliang <142937141+jinchaoliang@users.noreply.github.com>
2024-04-18 00:10:39 -0600

    new

--- modelformaster.cpp
+++
@@ -1,9 +0,0 @@
-#include "modelformaster.h"
-
-ModelForMaster::ModelForMaster(QObject *parent): QObject(parent) {
-    currentLevel = 1;
-    player = new Block("Player");
-    wall = new Block("Wall");
-    for(int i = 0; i < 5;i++)
-        levels[i] = new Map(i);
-}
